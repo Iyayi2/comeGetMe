1a483711df0d17e3c1a41e3307997ff005c64dd3
Iyayi Roland <iyayiroland@gmail.com>
2024-03-10 11:03:08 +0100

    added user and product models

---
+++ models/product.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const productSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User'
+  }
+});
+
+module.exports = mongoose.model('Product', productSchema);
