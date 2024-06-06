2116bebdd23d8ce431983c9ce4a90a4affbb37f5
victor loginov <tkoe88@zoho.com>
2024-06-07 00:20:00 +0200

    can sign up for accounts - sent to backend

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,9 +1,28 @@
 import Input from './Input';
 import css from './Form.module.css'
+// import { useState } from 'react';
 
 export default function Form() {
+
+  const signup = async (event:  React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    const resData = await response.json();
+    console.log('data', data, '\n\n', 'response', resData)
+  };
+
   return (
-    <form className={css.form}>
+    <form className={css.form} onSubmit={signup}>
       <Input id='username' />
       <Input id='email' />
       <Input id='password' />
