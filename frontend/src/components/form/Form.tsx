9fd8e6b0292139f643d6bb956b6f3906d0ada42b
victor loginov <tkoe88@zoho.com>
2024-06-07 12:34:10 +0200

    created radio buttons to change between signup/login actions

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,15 +1,21 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 import Input from './Input';
-import css from './Form.module.css'
-// import { useState } from 'react';
+import css from './Form.module.css';
 
 export default function Form() {
+  const [formState, setFormState] = useState('signup');
 
-  const signup = async (event:  React.FormEvent<HTMLFormElement>) => {
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormState(event.target.value);
+  };
+
+  const submitHandler = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
     const data = Object.fromEntries(formData.entries());
 
-    const response = await fetch('http://localhost:3000/signup', {
+    const response = await fetch(`http://localhost:3000/${formState}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -18,15 +24,26 @@ export default function Form() {
     });
 
     const resData = await response.json();
-    console.log('data', data, '\n\n', 'response', resData)
+    console.log('data', data, '\n\n', 'response', resData, '\n\n', 'formState', formState);
   };
 
+  console.log('formState', formState);
+
   return (
-    <form className={css.form} onSubmit={signup}>
-      <Input id='username' />
+    <motion.form
+      className={css.form}
+      onSubmit={submitHandler}
+      key={formState}
+      initial={{ opacity: 0, x: -100 }}
+      animate={{ opacity: 1, x: 0 }}
+    >
+      {/* prettier-ignore */}
+      <input type='radio' value='signup' name='formSwitch' onChange={changeHandler} defaultChecked={formState === 'signup'} /> Sign Up
+      <input type='radio' value='login' name='formSwitch' onChange={changeHandler} defaultChecked={formState === 'login'} /> Login
+      {formState === 'signup' && <Input id='username' />}
       <Input id='email' />
       <Input id='password' />
-      <button>Submit</button>
-    </form>
+      <button>{formState.toUpperCase()}</button>
+    </motion.form>
   );
 }
