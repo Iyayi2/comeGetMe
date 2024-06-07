a10b4a832b9df086da17849bc2323296ab6fe19e
victor loginov <tkoe88@zoho.com>
2024-06-07 18:35:46 +0200

    animated form transition

--- frontend/src/components/form/Form.tsx
+++ frontend/src/components/form/Form.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import Input from './Input';
 import css from './Form.module.css';
 import { fetchData } from '../../util/fetchData';
 
 export default function Form() {
   const [formState, setFormState] = useState('signup');
+  const signup = formState === 'signup';
 
   const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     setFormState(event.target.value);
@@ -19,30 +20,41 @@ export default function Form() {
     console.log('data', data, '\n\n', 'response', resData, '\n\n', 'formState', formState);
   };
 
+  const animateProps = { opacity: 0, x: signup ? 100 : -100 };
   const radioProps = { type: 'radio', name: 'toggleForm', onChange: changeHandler };
+  const buttonProps = {
+    background: signup ? '#538392' : '#ADD899',
+    textShadow: signup ? '1px 1px 2px #000' : '',
+    color: signup ? '#FFFFFF' : '',
+  };
 
   console.log('formState', formState);
 
   return (
-    <motion.form
-      className={css.form}
-      onSubmit={submitHandler}
-      key={formState}
-      initial={{ opacity: 0, x: -100 }}
-      animate={{ opacity: 1, x: 0 }}
-    >
+    <form className={css.form} onSubmit={submitHandler}>
       <div className={css['radio-buttons']}>
         <label>
-          <input value='signup' {...radioProps} defaultChecked={formState === 'signup'} /> Sign Up
+          <input value='signup' {...radioProps} defaultChecked={signup} /> Sign Up
         </label>
         <label>
-          <input value='login' {...radioProps} defaultChecked={formState === 'login'} /> Login
+          <input value='login' {...radioProps} defaultChecked={!signup} /> Login
         </label>
       </div>
-      {formState === 'signup' && <Input id='username' />}
-      <Input id='email' />
-      <Input id='password' />
-      <button>{formState.toUpperCase()}</button>
-    </motion.form>
+      <AnimatePresence mode='wait'>
+        <motion.div
+          key={formState}
+          className={css.inputs}
+          initial={{ ...animateProps }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ ...animateProps }}
+          transition={{ ease: 'easeIn', duration: 0.3 }}
+        >
+          {formState === 'signup' && <Input id='username' />}
+          <Input id='email' />
+          <Input id='password' />
+          <button style={{ ...buttonProps }}>{formState.toUpperCase()}</button>
+        </motion.div>
+      </AnimatePresence>
+    </form>
   );
 }
