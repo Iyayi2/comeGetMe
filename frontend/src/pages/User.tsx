32ca0a884a27a940b16c7d08076acd7dd6a7910c
victor loginov <tkoe88@zoho.com>
2024-06-11 01:46:32 +0200

    re-render page on logout works

--- frontend/src/pages/User.tsx
+++ frontend/src/pages/User.tsx
@@ -4,22 +4,33 @@ import { useFetch } from '@/hooks/useFetch';
 import Product from '@/models/Product';
 import User from '@/models/User';
 import { useHTTP } from '@/hooks/useHTTP';
+import { useEffect, useState } from 'react';
 
 export default function UserPage() {
   const { data: products } = useFetch('products');
   const { data: user } = useFetch('login');
   const { isLoading, sendRequest } = useHTTP();
+  const [isLoggedIn, setIsLoggedIn] = useState(user);
+
+  useEffect(() => {
+    setIsLoggedIn(user);
+  }, [user]);
 
   let content;
 
-  if (user) {
-    const userItems = (products || []).filter((item: Product) => item.userId === (user as User)._id);
+  const handleLogout = async () => {
+    await sendRequest({ path: 'logout', method: 'POST' });
+    setIsLoggedIn(null);
+  };
+
+  if (isLoggedIn) {
+    const userItems = (products || []).filter((item: Product) => item.userId === (isLoggedIn as User)._id);
 
     content = (
       <>
         <Products products={userItems} />
         <button onClick={() => sendRequest({ path: 'login', method: 'GET' })}>{isLoading ? 'sending...' : 'check login'}</button>
-        <button onClick={() => sendRequest({ path: 'logout', method: 'POST' })}>{isLoading ? 'sending...' : 'check logout'}</button>
+        <button onClick={handleLogout}>{isLoading ? 'sending...' : 'check logout'}</button>
       </>
     );
   } else {
