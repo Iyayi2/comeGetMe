a2f1baf00fccbaf74fd97dc0c6ca27ff6fd7a940
victor loginov <tkoe88@zoho.com>
2024-08-24 17:11:53 +0200

    updated conversations controllers. seller/buyer conversations display to both

--- backend/controllers/conversation.js
+++ backend/controllers/conversation.js
@@ -1,40 +1,48 @@
+const mongoose = require('mongoose');
 const Conversation = require('../models/conversation');
 const { userDetails } = require('../util/userDetails');
 
 exports.postConversation = (req, res, next) => {
   const user = userDetails(req.session.user);
+  const seller = {
+    ...req.body.seller,
+    _id: new mongoose.Types.ObjectId(String(req.body.seller._id)), // Convert to ObjectId
+  }; // seller.product._id is non ObjectId, convert later if needed
   const newConversation = new Conversation({
-    members: [user, req.body.seller]
+    members: [user, seller],
   });
 
-  newConversation.save()
-  .then(newConversation => {
-    res.status(200).json(newConversation);
-  })
-  .catch(err => {
-    res.status(500).json(err);
-  })
+  newConversation
+    .save()
+    .then((newConversation) => {
+      res.status(200).json(newConversation);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 exports.getConversation = (req, res, next) => {
-  const user = userDetails(req.session.user);
-  Conversation.find({members: { $in: [user] }})
-  .then(conversation => {
-    res.status(200).json(conversation);
+  const { _id } = req.session.user;
+  Conversation.find({
+    members: { $elemMatch: { _id } },
   })
-  .catch(err => {
-    res.status(500).json(err);
-  })
+    .then((conversation) => {
+      res.status(200).json(conversation);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
 
 exports.getConversations = (req, res, next) => {
   Conversation.findOne({
-    members: { $all: [req.params.firstUserId, req.params.secondUserId] }
+    members: { $all: [req.params.firstUserId, req.params.secondUserId] },
   })
-  .then(conversations => {
-    res.status(200).json(conversations);
-  })
-  .catch(err => {
-    res.status(500).json(err);
-  })
+    .then((conversations) => {
+      res.status(200).json(conversations);
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 };
--- backend/controllers/market.js
+++ backend/controllers/market.js
@@ -21,18 +21,14 @@ exports.getPopulatedProducts = (req, res, next) => {
     });
 };
 
+// view json on http://localhost:3000/
 exports.getIndex = (req, res, next) => {
   Product.find()
     .then((products) => {
       res.status(200).json(products);
-      // res.render('market/index', {
-      //   Products: products,
-      //   pageTitle: 'Market',
-      //   path: '/'
-      // });
     })
     .catch((err) => {
       res.status(500).json(err);
-      // console.log(err);
+      console.log(err);
     });
 };
