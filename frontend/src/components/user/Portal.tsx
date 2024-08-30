586db3a9824b9cf5fba04a981fb1e1e99c168040
victor loginov <tkoe88@zoho.com>
2024-08-30 16:41:34 +0200

    portal component broken down

--- frontend/src/components/user/Portal.tsx
+++ frontend/src/components/user/Portal.tsx
@@ -1,15 +1,13 @@
-import { useContext, useState } from 'react';
+import { useState } from 'react';
 import { motion } from 'framer-motion';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useFetch } from '@/hooks/useFetch';
 import { useHTTP } from '@/hooks/useHTTP';
-import { Context } from '@/store/Context';
 import User from '@/models/User';
 import Product from '@/models/Product';
 import Products from '../products/Products';
-import Button from '../button/Button';
 import ItemForm from '../form/ItemForm';
 import css from './Portal.module.css';
+import UserInfo from './UserInfo';
 
 export default function Portal({
   user,
@@ -20,11 +18,9 @@ export default function Portal({
   onLogout: () => void;
   isLoading: boolean;
 }) {
-  const { username, email } = user;
   const { sendRequest, isLoading: sendingData, error } = useHTTP();
   const { data: userItems, setData, isLoading: isFetching } = useFetch<Product[]>('my-products');
   const [expanded, setExpanded] = useState(false);
-  const { navTo } = useContext(Context);
 
   const hasItems = userItems && userItems.length > 0;
 
@@ -44,44 +40,26 @@ export default function Portal({
       initial={{ y: -100 }}
       animate={{ y: 0, transition: { ease: 'easeIn', duration: 0.5 } }}
     >
-      <section>
-        <div className={css.info}>
-          <motion.h2
-            initial={{ opacity: 0, x: -100, scaleY: 0 }}
-            animate={{ opacity: 1, x: 0, scaleY: 1 }}
-            transition={{ duration: 0.5, delay: 0.8 }}
-          >
-            Welcome {username}
-          </motion.h2>
-          <p>
-            <FontAwesomeIcon icon='envelope' /> Your Email: {email}
-          </p>
-          <p>
-            <FontAwesomeIcon icon='signs-post' /> Ads Online: {(userItems || []).length}
-          </p>
-        </div>
-        <div className={css.buttons}>
-          <Button
-            text='logout'
-            style={{ background: '#cd4f25' }}
-            onClick={onLogout}
-            isLoading={isLoading}
-          />
-          <Button
-            text={'inbox'}
-            style={{ background: '#94ca7c' }}
-            onClick={() => navTo('/inbox')}
-          />
-          <Button
-            text={expanded ? 'cancel' : 'new ad'}
-            style={{ background: expanded ? '#747272' : '#538392' }}
-            onClick={() => setExpanded((toggle) => !toggle)}
-          />
-        </div>
-      </section>
-
-      <ItemForm expanded={expanded} dataFn={submitHandler} isLoading={sendingData} error={error} />
-      <Products onUserPage products={userItems || []} hasItems={hasItems} isLoading={isFetching} />
+      <UserInfo
+           expanded={expanded}
+        setExpanded={setExpanded}
+               user={user}
+           onLogout={onLogout}
+          isLoading={isLoading}
+          adsOnline={(userItems || []).length}
+      />
+      <ItemForm
+         expanded={expanded}
+           dataFn={submitHandler}
+        isLoading={sendingData}
+            error={error}
+      />
+      <Products
+        onUserPage
+          products={userItems || []}
+          hasItems={hasItems}
+         isLoading={isFetching}
+      />
     </motion.div>
   );
 }
---
+++ frontend/src/components/user/UserInfo.tsx
@@ -0,0 +1,60 @@
+import { motion } from 'framer-motion';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Dispatch, SetStateAction, useContext } from 'react';
+import { Context } from '@/store/Context';
+import Button from '../button/Button';
+import User from '@/models/User';
+import css from './UserInfo.module.css';
+
+export default function UserInfo({
+  user,
+  onLogout,
+  isLoading,
+  adsOnline,
+  expanded,
+  setExpanded,
+}: {
+         user: User;
+     onLogout: () => void;
+    isLoading: boolean;
+    adsOnline: number;
+     expanded: boolean;
+  setExpanded: Dispatch<SetStateAction<boolean>>;
+}) {
+  const { username, email } = user;
+  const { navTo } = useContext(Context);
+
+  return (
+    <section className={css['container']}>
+      <div className={css['info']}>
+        <motion.h2
+             initial={{ opacity: 0, x: -100, scaleY: 0 }}
+             animate={{ opacity: 1, x:    0, scaleY: 1 }}
+          transition={{ duration: 0.5, delay: 0.8 }}
+        >
+          Welcome {username}
+        </motion.h2>
+        <p>
+          <FontAwesomeIcon icon='envelope'  /> Your Email: {email}
+        </p>
+        <p>
+          <FontAwesomeIcon icon='signs-post' /> Ads Online: {adsOnline}
+        </p>
+      </div>
+      <div className={css['buttons']}>
+        <Button
+               text='logout'
+              style={{ background: '#cd4f25' }}
+            onClick={onLogout}
+          isLoading={isLoading}
+        />
+        <Button text={'inbox'} style={{ background: '#94ca7c' }} onClick={() => navTo('/inbox')} />
+        <Button
+             text={              expanded ?  'cancel' : 'new ad'   }
+            style={{ background: expanded ? '#747272' : '#538392' }}
+          onClick={() => setExpanded((toggle) => !toggle)}
+        />
+      </div>
+    </section>
+  );
+}
