da3fe248adb2a49a4f50e34d9bf1430814053a40
victor loginov <tkoe88@zoho.com>
2024-08-30 18:41:14 +0200

    created dynamic navbar inbox button for logged in users

--- frontend/src/components/navigation/NavButton.tsx
+++ frontend/src/components/navigation/NavButton.tsx
@@ -7,14 +7,14 @@ import css from './Navigation.module.css';
 export default function NavButton({ path, label }: { path: string; label: string }) {
   const { pathname } = useLocation();
   const { isAnimating, navTo } = useContext(Context);
-  const isActive =
-    pathname === path ||
-    (pathname.startsWith(path) && path !== '/') ||
-    (pathname.startsWith ('/inbox') && path === '/account');
-
+  const isActive = pathname === path || (pathname.startsWith(path) && path !== '/');
 
   return (
-    <li>
+    <motion.li
+       variants={{ hidden: { opacity: 0 }, visible: { opacity: 1 } }}
+           exit={{ opacity: 0 }}
+      transition={{ duration: 0.5, ease: 'easeInOut' }}
+    >
       <button
         style={isAnimating ? { pointerEvents: 'none', opacity: 0.6 } : {}}
         onClick={() => navTo(path)}
@@ -22,6 +22,6 @@ export default function NavButton({ path, label }: { path: string; label: string
         {label}
       </button>
       {isActive && <motion.div layoutId='tab-indicator' className={css['active-tab']} />}
-    </li>
+    </motion.li>
   );
 }
