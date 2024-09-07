a16c7c28d32c1d1602d9024aca3bf35e5c61b07e
victor loginov <tkoe88@zoho.com>
2024-09-07 18:00:11 +0200

    updated imagefocus component

--- frontend/src/components/home/ImageFocus.tsx
+++ frontend/src/components/home/ImageFocus.tsx
@@ -1,15 +1,19 @@
 import woman from '@/assets/pngs/autumn-woman.png';
+import leaves from '@/assets/pngs/leaves.png';
+import balloons from '@/assets/pngs/balloons.png';
 import css from './ImageFocus.module.css';
 
 export default function ImageFocus() {
   return (
     <div className={css['image-focus']}>
       <p>
+      <img src={balloons} alt='leaves' />
         <span>Advertise</span>
         <span>Freely</span>
       </p>
       <img src={woman} alt='woman in autumn' />
       <p>
+        <img src={leaves} alt='leaves' />
         <span>Reach Your</span>
         <span>Audience</span>
       </p>
